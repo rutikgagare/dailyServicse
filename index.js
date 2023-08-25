ebced3b372bae795b8660cd9450a5c4d07e21747
Rutik Gagare <rutikgagare091971@gmail.com>
2023-08-25 22:34:11 +0530

    changes

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-let displayElectric = fetch('electrician.json').then((Response)=>{
-    return Response.json();
-}).then((data)=>{
-    console.log(data);
+// let displayElectric = fetch('electrician.json').then((Response)=>{
+//     return Response.json();
+// }).then((data)=>{
+//     console.log(data);
 
-    let x = document.querySelector('.display');
+//     let x = document.querySelector('.display');
 
-    data.map((item)=>{
-        x.innerHTML+=`<div class="item">
-            <h3>${item.title}</h3>
-            <h4>Rating : ${item.rating} ✰</h4>
-            <h4>Price : ${item.price}</h4>
-        </div>`
-    });
-});
+//     data.map((item)=>{
+//         x.innerHTML+=`<div class="item">
+//             <h3>${item.title}</h3>
+//             <h4>Rating : ${item.rating} ✰</h4>
+//             <h4>Price : ${item.price}</h4>
+//         </div>`
+//     });
+// });
 
-let displayPainter = fetch('electrician.json').then((Response)=>{
-    return Response.json();
-}).then((data)=>{
-    console.log(data);
+// let displayPainter = fetch('electrician.json').then((Response)=>{
+//     return Response.json();
+// }).then((data)=>{
+//     console.log(data);
 
-    let x = document.querySelector('.display');
+//     let x = document.querySelector('.display');
 
-    data.map((item)=>{
-        x.innerHTML+=`<div class="item">
-            <h3>${item.title}</h3>
-            <h4>Rating : ${item.rating} ✰</h4>
-            <h4>Price : ${item.price}</h4>
-        </div>`
-    });
-});
+//     data.map((item)=>{
+//         x.innerHTML+=`<div class="item">
+//             <h3>${item.title}</h3>
+//             <h4>Rating : ${item.rating} ✰</h4>
+//             <h4>Price : ${item.price}</h4>
+//         </div>`
+//     });
+// });
 
-let displayPlumber = fetch('electrician.json').then((Response)=>{
-    return Response.json();
-}).then((data)=>{
-    console.log(data);
+// let displayPlumber = fetch('electrician.json').then((Response)=>{
+//     return Response.json();
+// }).then((data)=>{
+//     console.log(data);
 
-    let x = document.querySelector('.display');
+//     let x = document.querySelector('.display');
 
-    data.map((item)=>{
-        x.innerHTML+=`<div class="item">
-            <h3>${item.title}</h3>
-            <h4>Rating : ${item.rating} ✰</h4>
-            <h4>Price : ${item.price}</h4>
-        </div>`
-    });
-});
+//     data.map((item)=>{
+//         x.innerHTML+=`<div class="item">
+//             <h3>${item.title}</h3>
+//             <h4>Rating : ${item.rating} ✰</h4>
+//             <h4>Price : ${item.price}</h4>
+//         </div>`
+//     });
+// });
